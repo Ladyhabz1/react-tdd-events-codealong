2c4ffba98d101dbe05f501671cd84242fbe61d01
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-08 12:19:02 +0300

    complete labs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App() {
-  return (
-    <div></div>
-  )
-}
+import { useState } from "react";
 
-export default App;
+export default function App() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div>
+      <h1>Pizza Order</h1>
+      <label>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+        Add Pepperoni
+      </label>
+      <h2>Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {isChecked && <li>Pepperoni</li>}
+      </ul>
+    </div>
+  );
+}
